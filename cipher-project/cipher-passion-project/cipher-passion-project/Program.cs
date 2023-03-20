6529f0c2914c994f47cd52e3d05dc30c76545284
Michael McGhen <mcghenmike@gmail.com>
2023-03-20 17:38:53 -0400

    Updated to include a decrytpion service

--- cipher-project/cipher-passion-project/cipher-passion-project/Program.cs
+++ cipher-project/cipher-passion-project/cipher-passion-project/Program.cs
@@ -4,15 +4,47 @@ class ShiftCipher
 {
     static void Main()
     {
-        Console.Write("Enter message to encrypt: ");
-        string message = Console.ReadLine();
+        string selection;
+        do
+        {
+            Console.WriteLine("Would you like to:");
+            Console.WriteLine("1) Enter a message to encrypt");
+            Console.WriteLine("2) Enter a message to decrypt");
+            Console.WriteLine("3) Exit");
+            selection = Console.ReadLine();
+            Console.WriteLine();
+            if(selection == "1")
+            {
+                Console.Write("Enter message to encrypt: ");
+                string message = Console.ReadLine();
 
-        Console.Write("Enter shift value: ");
-        int shift = int.Parse(Console.ReadLine());
+                Console.Write("Enter shift value: ");
+                int shift = int.Parse(Console.ReadLine());
+                string encryptedMessage = Encrypt(message, shift);
+                Console.WriteLine($"Encrypted message: {encryptedMessage}");
+                Console.WriteLine();
+            }
+            else if (selection == "2")
+            {
+                Console.Write("Enter message to decrypt: ");
+                string message = Console.ReadLine();
 
-        string encryptedMessage = Encrypt(message, shift);
-
-        Console.WriteLine($"Encrypted message: {encryptedMessage}");
+                Console.Write("Enter shift value: ");
+                int shift = int.Parse(Console.ReadLine());
+                string decryptedMessage = Decrypt(message, shift);
+                Console.WriteLine($"Decrypted message: {decryptedMessage}");
+                Console.WriteLine();
+            }
+            else if (selection == "3")
+            {
+                Console.WriteLine("Thank you, have a nice day!");
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid input.");
+            }
+        }
+        while (selection != "3");   
     }
 
     static string Encrypt(string message, int shift)
@@ -36,5 +68,27 @@ class ShiftCipher
             }
         }
         return encrypted;
+    }
+    static string Decrypt(string message, int shift)
+    {
+        string decrypted = "";
+        foreach (char c in message)
+        {
+            if (char.IsLetter(c) && c >= 65 && c <= 90)
+            {
+                char shifted = (char)((char.ToUpper(c) - 65 - shift) % 26 + 65);
+                decrypted += shifted;
+            }
+            else if (char.IsLetter(c))
+            {
+                char shifted = (char)((char.ToUpper(c) - 65 - shift) % 26 + 65);
+                decrypted += char.ToLower(shifted);
+            }
+            else
+            {
+                decrypted += c;
+            }
+        }
+        return decrypted;
     }
 }
