7861298b1ec61be63f5f244c045839d3c87a8450
Michael McGhen <mcghenmike@gmail.com>
2023-03-20 16:54:06 -0400

    Initial Commit

---
+++ News Grabber/News Grabber/Program.cs
@@ -0,0 +1,43 @@
+﻿using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // init with your API key
+            var newsApiClient = new NewsApiClient("9a9427da64c5451dbec7db5933e61488");
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = "Apple",
+                SortBy = SortBys.Popularity,
+                Language = Languages.EN,
+                From = new DateTime(2018, 1, 25)
+            });
+            if (articlesResponse.Status == Statuses.Ok)
+            {
+                // total results found
+                Console.WriteLine(articlesResponse.TotalResults);
+                // here's the first 20
+                foreach (var article in articlesResponse.Articles)
+                {
+                    // title
+                    Console.WriteLine(article.Title);
+                    // author
+                    Console.WriteLine(article.Author);
+                    // description
+                    Console.WriteLine(article.Description);
+                    // url
+                    Console.WriteLine(article.Url);
+                    // published at
+                    Console.WriteLine(article.PublishedAt);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
